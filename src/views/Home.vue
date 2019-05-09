3161d049ef8bd08994712cc68070980c9e2e391a
tanlinlin <tanlinlin@xiaozhu.com>
2019-05-09 21:51:27 +0800

    增加备注

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,22 +11,28 @@
     @click="open2">
     不会自动关闭
     </el-button>
-      <h5>下面这些就是一个插件”element-ui“ </h5>
+      <h5>下面这些就是el开头的是一个插件”element-ui“,<a href="https://element.eleme.cn/#/zh-CN/component/layout">插件地址</a> </h5>
+      <!-- 进度条组件 -->
       <el-progress :percentage="70"></el-progress>
       <el-progress :percentage="80" color="#8e71c7"></el-progress>
       <el-progress :percentage="100" status="success"></el-progress>
       <el-progress :percentage="50" status="exception"></el-progress>
+       <!-- 冒号”:“代表是vue的v-bind的简写，表示数据绑定  -->
+       <!-- @是v-on的简写，表示绑定时间，例如handleNodeClick是在methods中定义的  -->
+      <!-- tree组件 -->
       <el-tree :data="dataMenu" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
-
+      <div class="box">我是box</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+// 引用components中的组件
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
+  // 上面引用的组件要在这里注册，才能使用
   components: {
     HelloWorld
   },
@@ -76,6 +82,7 @@ export default {
         }],
     }
   },
+  //在这里面写方法 
   methods: {
     handleNodeClick(data) {
         console.log(data);
@@ -99,11 +106,15 @@ export default {
   },
 }
 </script>
+// 这个less是一个插件，这样的话写样式就好写一些，
+// scoped表示以下样式只对这个页面的元素起作用，不会污染别的页面
+
 <style lang="less" scoped>
-  .progress{
+  .box{
     width:500px;
     height:100px;
     border:solid 1px #eee;
     border-radius : 5px;
+    background: #bdde95;
   }
 </style>
